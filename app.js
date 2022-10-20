bb3c70884da77ed33cde960b09e4e18da1549055
Tomas Alfonso <tomasalfonsomassone@gmail.com>
2022-10-20 13:15:03 -0300

    Estilos facheritos de la tabla y modo oscuro

--- app.js
+++ app.js
@@ -1,117 +1,121 @@
 //crear lista con personas
-personas = []
+personas = [];
 
-var socket
+var socket;
 
 //cada uno debe poner sus datos aqui
 const Me = {
-    nombre: "Matias",
-    apellido: "Endres"
-} 
+  nombre: "Matias",
+  apellido: "Endres",
+};
 
 //traigo el componente boton
-const button = document.querySelector("#btnSubmit")
+const formulario = document.querySelector("#formulario");
 
-//Creo un evento para el boton
-button.addEventListener('click', () => {
-    //traer informacion de formulario
-    persona = {
-        id : Date.now(),
-        nombre : document.querySelector("#inputNombre").value,
-        apellido : document.querySelector("#inputApellido").value,
-        fecha : document.querySelector("#inputFecha").value,
-        color : document.querySelector("#inputColor").value,
-        email : document.querySelector("#inputEmail").value
-    }
+//evento de submit en el formulario
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    socket.emit("new", persona, Me)
-})
+  //traer informacion de formulario
+  persona = {
+    id: Date.now(),
+    nombre: document.querySelector("#inputNombre").value,
+    apellido: document.querySelector("#inputApellido").value,
+    fecha: document.querySelector("#inputFecha").value,
+    color: document.querySelector("#inputColor").value,
+    email: document.querySelector("#inputEmail").value,
+  };
+
+  socket.emit("new", persona, Me);
+});
 
 //creo una funcion para el boton
-function Agregar(persona){
+function Agregar(persona) {
+  personas.push(persona);
 
-    personas.push(persona)
+  //traer el elemento tabla
+  var tabla = document.querySelector("#tableContent");
 
-    //traer el elemento tabla
-    var tabla = document.querySelector("#tableContent")
+  //creo elemento fila
+  var fila = document.createElement("tr");
 
-    //creo elemento fila
-    var fila = document.createElement('tr')
-    
-    //id de fila
-    fila.id = persona.id
+  //id de fila
+  fila.id = persona.id;
 
-    fila.innerHTML = `
+  fila.innerHTML = `
         <td>${persona.nombre}</td>
         <td>${persona.apellido}</td>
         <td>${persona.fecha}</td>
-        <td style="background-color: ${persona.color};"></td>
+        <td><button class="button-round" style="background-color: ${persona.color};"></button></td>
         <td>${persona.email}</td>
-        <td><button class="btn btn-danger" onclick="Eliminar(${persona.id})">Eliminar</button></td>
-    `
+        <td><button class="button-round delete" onclick="Eliminar(${persona.id})"><i class="fa-solid fa-trash"></i></button></td>
+    `;
 
-    tabla.appendChild(fila)
+  tabla.appendChild(fila);
 }
 
-function Eliminar(id){
-    socket.emit("delete", id, Me)
+function Eliminar(id) {
+  socket.emit("delete", id, Me);
 }
-
 
 //evento chkTema
-var checkbox = document.querySelector("#chkTema")
-checkbox.addEventListener('change', cambiarTema)
+var btnTema = document.querySelector("#btnTema");
+btnTema.addEventListener("click", cambiarTema);
+var checkbox = document.querySelector("#chkTema");
 
-function cambiarTema(){
-    var root = document.querySelector(":root")
-    if(checkbox.checked){
-        //poner el tema en oscuro
-        root.style.setProperty('--color1','#202020')
-        root.style.setProperty('--color2','#5d5d5d')
-        root.style.setProperty('--color3','#313131')
-        root.style.setProperty('--color4','#ffffff')
-    }else{
-        //poner el tema claro
-        root.style.setProperty('--color1','#ffffff')
-        root.style.setProperty('--color2','#dfdfdf')
-        root.style.setProperty('--color3','#808080')
-        root.style.setProperty('--color4','#000000')
-    }
+function cambiarTema() {
+  checkbox.checked = !checkbox.checked;
+  var root = document.querySelector(":root");
+  if (checkbox.checked) {
+    //poner el tema en oscuro
+    root.style.setProperty("--primary", "#5d5d5d");
+    root.style.setProperty("--primary-light", "#5d5d5d");
+    root.style.setProperty("--light", "#313131");
+    root.style.setProperty("--dark", "#eaf4f4");
+    root.style.setProperty("--background", "#3a3a3c");
+    root.style.setProperty("--white", "#3a3a3c");
+    root.style.setProperty("--gray", "#1c1c1e");
+  } else {
+    //poner el tema claro
+    root.style.setProperty("--primary", "#a4c3b2");
+    root.style.setProperty("--primary-light", "#cce3de");
+    root.style.setProperty("--light", "#eaf4f4");
+    root.style.setProperty("--dark", "#6b9080");
+    root.style.setProperty("--background", "#f6fff8");
+    root.style.setProperty("--white", "#f6fff8");
+    root.style.setProperty("--gray", "#c7cfcf");
+  }
 }
 
 //traer datos de la temperatura
-fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/villa%20mercedes?unitGroup=metric&key=K9KNX4P6YGSNXCJEH4W348VBW&contentType=json", {
-  "method": "GET",
-  "headers": {
+fetch(
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/villa%20mercedes?unitGroup=metric&key=K9KNX4P6YGSNXCJEH4W348VBW&contentType=json",
+  {
+    method: "GET",
+    headers: {},
   }
-  })
-.then(response => response.json())
-.then(data => {
-    temperatura = data.days[0].temp
+)
+  .then((response) => response.json())
+  .then((data) => {
+    temperatura = data.days[0].temp;
 
     /* alert(`La temperatura es: ${temperatura}`) */
-})
-.catch(err => {
-  console.error(err);
-});
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 window.onload = () => {
-    socket = io('http://localhost:3000/', {
-        withCredentials: false,
-    });
+  socket = io("http://192.168.1.107:3000/", {
+    withCredentials: false,
+  });
 
-    socket.on("recive new", data => {
-        console.log(data)
-        Agregar(data.item)
-    });
+  socket.on("recive new", (data) => {
+    Agregar(data.item);
+  });
 
-    socket.on("recive delete", data => {
-        console.log(data)
-        var fila = document.getElementById(data.itemID)
-        fila.remove()
-    });
-}
-
-
-
-
+  socket.on("recive delete", (data) => {
+    var fila = document.getElementById(data.itemID);
+    fila.remove();
+  });
+};
